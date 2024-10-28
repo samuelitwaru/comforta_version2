a76e01246865d50ac596016476c3b9ef3e508f81
samuelitwaru <samuelitwaru@gmail.com>
2024-10-28 07:19:24 +0300

    toolbox test

--- Resources/UCGrapes/new-design/js/toolbox.js
+++ Resources/UCGrapes/new-design/js/toolbox.js
@@ -60,7 +60,7 @@ class EditorManager {
       }
 
       const wrapper = this.editor.getWrapper();
-      console.dir(wrapper.view.el)
+      console.dir(wrapper.view)
       wrapper.view.el.addEventListener("click", (e) => {
         const button = e.target.closest(".action-button");
         if (!button) return;
